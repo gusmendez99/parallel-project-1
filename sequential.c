606ac9063051dc713f8bc2e1c3032aa9141a4ee5
RobertoFigueroa <fig18306@uvg.edu.gt>
2022-03-16 22:53:56 -0600

    [FIX] sequential program

--- sequential.c
+++ sequential.c
@@ -8,9 +8,11 @@ Sequential code for the calculation of heat dissipation
 #include <stdlib.h>
 #include <time.h>
 
-#define  N 10 
+#define  N 5000
+int calculateT(int n, double *uk1, double *uk0,
+double alpha, double dx, double dt, double bc0, double bc1, double * condition);
+double check_condition(double * new, double * old, int size);
 
-double calculateT(int pos, double rod[N] ,double C, double lapse);
 int main(int argc, char* argv[]){
 
     time_t start, end;
@@ -18,47 +20,86 @@ int main(int argc, char* argv[]){
     //start = clock();
     //end = clock();
     //cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-    double rod[N];
+    double * rod = (double *) malloc(N * sizeof(double));
+    double * rod_1 = (double *) malloc(N * sizeof(double));
     double err = 10e-200;
-    double T0 = 27;
+    double T0 = 20;
     double Tl = 60;
-    double Tr = 140;
-    double L = 100;
-    // double C = 10e-5;
-    double C = 0.5;
+    double Tr = 120;
+    double L = 10;
+    double C = 9e-3;
+    //double C = 0.5;
 
     //Fill the array with the first temp
     for(int i = 0; i < N; ++i){
         rod[i] = T0;
+        rod_1[i] = T0;
     }
 
     //Set the frontier conditions
     rod[0] = Tl;
     rod[N-1] = Tr;
+    rod_1[0] = Tl;
+    rod_1[N-1] = Tr;
 
 
     //For now the code runs for a set amount of time, should change it to a different stop condition
-    int iters = 0;
-    start = time(0);
-    while(iters < 100000){
-        for(int j = 1; j < N -1; ++j){
-            // end = time(0);
-            // lapsed_time = difftime(end, start);
-            double oldT = rod[j];
-            double newT = calculateT(j, rod, C, lapsed_time);
-            printf("T in %d in time %f is %f\n", j, lapsed_time, newT);
-            rod[j] = newT;
-        }
+    double iters = 1;
+    double condition = 100;
+    double d_time = (0.5 / C)* (L/N) * (L/N);
+    while(condition > 1e-4){
+
+        calculateT(N, rod_1, rod, C, (L/N), d_time, Tl, Tr, (double *) &condition);
+        //printf("Iter: %f Condition: %f \n", iters, condition);
         iters += 1;
+        //condition = check_condition(rod_1, rod, N);
     }
 
     // printing final state
-    for( int k = 0; k < N; ++k){
-        printf("%f\n", rod[k]);
+    for( int k = 0; k <= N; k+=100){
+        printf("%f\n", rod_1[k]);
     }
+
+    free(rod_1);
+    free(rod);
 }
 
-double calculateT(int pos, double rod[N] ,double C, double lapse){
-    double t = rod[pos] + C * (lapse / (pos*pos)) * (rod[pos -1] - 2*rod[pos]+ rod[pos +1]);
-    return  t;
+double check_condition(double * new, double * old, int size) {
+    double max = sizeof(int);
+    max = -1 * max;
+    for(int i=0; i<=size-1; ++i) {
+        double val = new[i] - old[i];
+        if (val > max) {
+            max = val;
+        }
+    }
+    printf("---> %f\n", max);
+    return max;
+}
+
+int calculateT(int n, double *uk1, double *uk0,
+double alpha, double dx, double dt, double bc0, double bc1, double * condition){
+    double r = alpha * dt / (dx * dx);
+    // Sanity check for stability
+    if (r > 0.5) return 0;
+
+    //FTCS update algorithm
+    for(int i = 1; i < n-1; ++i) {
+        uk1[i] = uk0[i] + r * (uk0[i-1] -2* uk0[i] + uk0[i+1]);
+    }
+
+
+    //condition
+    
+    double cond_value = check_condition(uk1, uk0, n);
+    *condition = cond_value;
+
+    for(int i=1; i<n-1; ++i) {
+        uk0[i] = uk1[i];
+    }
+    // Enforce boundary conditions
+    uk1[0] = bc0;
+    uk1[n] = bc1;
+
+    return 1;
 }
